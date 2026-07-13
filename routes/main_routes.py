5c835659f2a625ace7ee0d69b8de8b2af8801189
Damiano de Lima <damdlm@users.noreply.github.com>
2026-07-13 19:53:56 -0300

    Tela de login

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 from services.treino_service import TreinoService
 from services.registro_service import RegistroService
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route("/")
+@login_required
 def index():
     """Página inicial"""
     treinos = TreinoService.get_all()
